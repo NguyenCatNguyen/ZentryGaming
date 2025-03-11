47749fa89a0171c73ba551e5f6b1da6222a50882
NguyenCatNguyen <catngn11@gmail.com>
2025-03-11 06:01:32 -0500

    update

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,8 +1,89 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import AnimatedTitle from './AnimatedTitle'
+import Button from './Button'
+import gsap from 'gsap';
 
 const Story = () => {
+  const frameRef = useRef("null");
+
+  const handleMouseLeave = () =>{
+    const element = frameRef.current;
+    gsap.to(element,{
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+      ease: "power1.inOut"
+    })
+  }
+
+  const handleMouseMove = (event) => {
+    const {clientX, clientY} = event;
+    const element = frameRef.current;
+    
+    if(!element) return;
+
+    const rect = element.getBoudingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+
+    const centerX = rect.width/2;
+    const centerY = rect.height/2;
+
+    const rotateX = (( y - centerY) / centerY) * -10;
+    const rotateY = (( x - centerX) / centerX) * 10;
+
+    gsap.to(element,{
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut"
+    })
+  }
+
   return (
-    <div>Story</div>
+    <section id="story" className='min-h-dvh w-screen bg-black text-blue-50'>
+      <div className='flex size-full flex-col items-center py-10 pb-24'>
+        <p className='font-general text-sm uppercase md:text-[10px]'>the multiversal ip world</p>
+      </div>
+
+      <div className='relative size-full'>
+        <AnimatedTitle 
+          title="The st<b>o</b>ry of a hidden real<b>m</b>"
+          sectionId="#story"
+          containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"/>
+          <div className='story-img-container'>
+            <div className='story-img-mask'>
+              <div className='story-img-content'>
+                <img src="/img/entrance.webp" alt="etrance" 
+                  className='object-contain'
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
+                  ref={frameRef}/>
+
+              </div>
+            </div>
+          </div>
+          <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end ">
+            <div className="flex h-full w-fit flex-col items-center md:items-start">
+              <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+                Where realms converge, lies Zentry and the boundless pillar.
+                Discover its secrets and shape your fate amidst infinite
+                opportunities.
+              </p>
+
+              <Button
+                id="realm-btn"
+                title="discover prologue"
+                containerClass="mt-5 mb-10"
+              />
+            </div>
+          </div>
+      </div>
+
+    </section>
   )
 }
 
